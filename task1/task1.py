9f92a3b93023bb56f3f4fb20fc79d1d3f953b318
Denis Solovev <denissolovev99@icloud.com>
2021-11-30 02:41:40 +0300

    entering arguments from the command line

--- task1/task1.py
+++ task1/task1.py
@@ -1,13 +1,18 @@
-n, m = int(input()), int(input())
+import sys
+
+n, m = map(int, (sys.argv[1], sys.argv[2]))
 lst = (n - 1) * [i for i in range(1, n + 1)] + [1]
 intervals = []
+
 while lst:
     interval = lst[:m]
     intervals.append(interval)
     if interval[-1] == 1:
         break
     lst = lst[m - 1:]
+
 result = ""
+
 for i in intervals:
     result += str(i[0])
 print(result)
