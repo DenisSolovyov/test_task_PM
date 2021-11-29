dc14aa5d061cb59252ab45261999ecf803fc7b91
Denis Solovev <denissolovev99@icloud.com>
2021-11-30 02:36:11 +0300

    test task completed

--- task2/task2.py
+++ task2/task2.py
@@ -1,17 +1,22 @@
-with open('file1.txt') as f1:
+import sys
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+with open(file1) as f1:
     x0, y0 = map(float, f1.readline().split())
     r = float(f1.readline())
 
-with open('file2.txt') as f2:
+with open(file2) as f2:
     points = []
     for line in f2.readlines():
         x, y = map(float, line.split())
         points.append((x, y))
 
 for point in points:
-    if (point[0] - x0)**2 + (point[1] - y0)**2 == r**2:
+    if (point[0] - x0) ** 2 + (point[1] - y0) ** 2 == r ** 2:
         print(0)
-    elif (point[0] - x0)**2 + (point[1] - y0)**2  < r**2:
+    elif (point[0] - x0) ** 2 + (point[1] - y0) ** 2 < r ** 2:
         print(1)
     else:
         print(2)
